7ed42a22327a3854bc62276fdf6079144aec15d0
y.el-haddady <youssef.el-haddady@nimbelways.com>
2022-04-15 01:03:35 +0000

    fix tests for service and controller after adding mongoose

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,6 +1,15 @@
 import { BooksService } from "./books.service";
 import { Test, TestingModule } from "@nestjs/testing";
 import { BooksController } from "./books.controller";
+import { getModelToken } from "@nestjs/mongoose";
+import { Book } from "./schemas/book.schema";
+
+const mockBook = {
+  id: 1,
+  title: "title #1",
+  description: "desc #1",
+  author: "auth #1",
+};
 
 describe("BooksController", () => {
   let controller: BooksController;
@@ -8,7 +17,15 @@ describe("BooksController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
-      providers: [BooksService],
+      providers: [BooksService, {
+        provide: getModelToken(Book.name), useValue: {
+          new: jest.fn().mockResolvedValue(mockBook),
+          constructor: jest.fn().mockResolvedValue(mockBook),
+          find: jest.fn(),
+          create: jest.fn(),
+          exec: jest.fn(),
+        },
+      }],
     }).compile();
 
     controller = module.get<BooksController>(BooksController);
